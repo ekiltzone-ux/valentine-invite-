408db6583ec1482a87f96833817de8e5ebfcb706
ekiltzone-ux <ekiltzone@gmail.com>
2026-01-21 16:19:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let messageIndex = 0;
 
 const messages = [
     "Are you sure?",
-    "Really sure??",
+    "Really sure?",
     "Are you positive?",
-    "Pleeasee...",
+    "Pleasee...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
-    "I will be very very very sad...",
+    "I will be very very sad...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
